015f79816caed4a24cbe6b4a17eb762f94a5145e
Shauren <shauren.trinity@gmail.com>
2019-11-26 23:20:11 +0100

    Core/Spells: Force creating only 1 enchanting scroll when targeting a vellum with enchantments
    # Conflicts:
    #	src/server/game/Spells/SpellEffects.cpp

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -2484,6 +2484,7 @@ void Spell::EffectEnchantItemPerm(SpellEffIndex effIndex)
         player->DestroyItemCount(itemTarget, count, true);
         unitTarget = player;
         // and add a scroll
+        damage = 1;
         DoCreateItem(effIndex, effectInfo->ItemType);
         itemTarget = NULL;
         m_targets.SetItemTarget(NULL);
